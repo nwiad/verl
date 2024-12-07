d1242b0acc95b39e41167c5c4f59e5efb4556273
daiweinan.thu <daiweinan.thu@bytedance.com>
2024-12-07 17:57:24 +0800

    fix batch size

--- run_gsm8k/run_gemma-2-2b-it.sh
+++ run_gsm8k/run_gemma-2-2b-it.sh
@@ -3,6 +3,11 @@ set -x
 WORK_DIR=/opt/tiger/dwn-verl
 MODEL=/mnt/bn/daiweinan-fuse/models/gemma-2-2b-it
 GPUS_PER_NODE=8
+ACTOR_MICRO_BS=16
+ROLLOUT_MICRO_BS=16
+ROLLOUT_TP=8
+REF_MICRO_BS=16
+CRITIC_MICRO_BS=16
 
 python3 -m verl.trainer.main_ppo \
     data.train_files=$WORK_DIR/run_gsm8k/gsm8k_ppo/train.parquet \
@@ -14,20 +19,20 @@ python3 -m verl.trainer.main_ppo \
     actor_rollout_ref.model.path=$MODEL \
     actor_rollout_ref.actor.optim.lr=1e-6 \
     actor_rollout_ref.actor.ppo_mini_batch_size=128 \
-    actor_rollout_ref.actor.ppo_micro_batch_size=4 \
+    actor_rollout_ref.actor.ppo_micro_batch_size=$ACTOR_MICRO_BS \
     actor_rollout_ref.actor.fsdp_config.param_offload=False \
     actor_rollout_ref.actor.fsdp_config.grad_offload=False \
     actor_rollout_ref.actor.fsdp_config.optimizer_offload=False \
-    actor_rollout_ref.rollout.log_prob_micro_batch_size=4 \
-    actor_rollout_ref.rollout.tensor_model_parallel_size=2 \
+    actor_rollout_ref.rollout.log_prob_micro_batch_size=$ROLLOUT_MICRO_BS \
+    actor_rollout_ref.rollout.tensor_model_parallel_size=$ROLLOUT_TP \
     actor_rollout_ref.rollout.name=vllm \
     actor_rollout_ref.rollout.gpu_memory_utilization=0.4 \
-    actor_rollout_ref.ref.log_prob_micro_batch_size=4 \
+    actor_rollout_ref.ref.log_prob_micro_batch_size=$REF_MICRO_BS \
     actor_rollout_ref.ref.fsdp_config.param_offload=True \
     critic.optim.lr=1e-5 \
     critic.model.path=$MODEL \
     critic.model.enable_gradient_checkpointing=False \
-    critic.ppo_micro_batch_size=4 \
+    critic.ppo_micro_batch_size=$CRITIC_MICRO_BS \
     critic.model.fsdp_config.param_offload=False \
     critic.model.fsdp_config.grad_offload=False \
     critic.model.fsdp_config.optimizer_offload=False \
