80e395b506617f839ce92498ddb8f08dee9a0c9f
daiweinan.thu <daiweinan.thu@bytedance.com>
2024-12-13 13:55:20 +0800

    adjust rollout logging

--- verl/trainer/ppo/rollout/vllm_rollout/vllm_rollout.py
+++ verl/trainer/ppo/rollout/vllm_rollout/vllm_rollout.py
@@ -170,10 +170,10 @@ class vLLMRollout(BaseRollout):
             }
 
         # users can customize different sampling_params at different run
-        print(f'batch size = {len(idx_list)}')
+        rank = torch.distributed.get_rank()
         lens = [len(x) for x in idx_list]
         mean_prompt_length = sum(lens) / len(lens)
-        print(f'mean prompt length = {mean_prompt_length:.2f}')
+        print(f'[Rank {rank}] batch size = {len(idx_list)}, mean prompt length = {mean_prompt_length:.2f}')
         with self.update_sampling_params(**kwargs):
             output = self.inference_engine.generate(
                 prompts=None,  # because we have already convert it to prompt token id
