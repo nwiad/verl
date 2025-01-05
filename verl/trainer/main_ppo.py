49d8132f153db457c8a57348e8d0ca781867e632
daiweinan.thu <daiweinan.thu@bytedance.com>
2025-01-05 23:17:39 +0800

    test runtime_env

--- verl/trainer/main_ppo.py
+++ verl/trainer/main_ppo.py
@@ -97,7 +97,7 @@ import hydra
 def main(config):
     if not ray.is_initialized():
         # this is for local ray cluster
-        ray.init(runtime_env={'env_vars': {'TOKENIZERS_PARALLELISM': 'true', 'NCCL_DEBUG': 'WARN'}})
+        ray.init(runtime_env={'env_vars': {'TOKENIZERS_PARALLELISM': 'true', 'NCCL_DEBUG': 'WARN', 'VLLM_ATTENTION_BACKEND': 'XFORMERS'}})
 
     ray.get(main_task.remote(config))
 
